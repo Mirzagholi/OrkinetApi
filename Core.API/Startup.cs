5faaa7e3c2c9c8e599723a28be17d48e9dec4018
jafarzadeh <jafarzadeh@gmail.com>
2023-10-15 15:58:20 +0330

    commit test ,edit by jafarzadeh

--- Core.API/Startup.cs
+++ Core.API/Startup.cs
@@ -26,7 +26,7 @@ namespace Core.API
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        // test
+        // test edit by jafarzadeh
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<SiteSettings>(options => Configuration.Bind(options));
